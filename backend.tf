b953f0b52e84950212b21f2dcd95f5395dc02e87
Abbeylincoln2022 <98928174+Abbeylincoln2022@users.noreply.github.com>
2022-08-04 18:01:57 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
     bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
+    key    = "Abbeylincoln/s3file-3.tfstate"
     region = "us-west-2"
     profile = "Abbeylincoln2022"
 dynamodb_table= "firstprojecttable"
